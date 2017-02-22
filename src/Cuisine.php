a381f29e0bf201e1c4800bb5df9084fed11f383a
Katharyn Reed and Patrick McGreevy <katharyn@>
2017-02-22 10:05:30 -0800

    Implement cuisine model

---
+++ src/Cuisine.php
@@ -0,0 +1,60 @@
+<?php
+class Cuisine
+{
+    private $name;
+    private $id;
+
+    function __construct($name, $id = null)
+    {
+        $this->name = $name;
+        $this->id = $id;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO cuisines (name) VALUES ('{$this->getName()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    static function getAll()
+    {
+        $returned_cuisines = $GLOBALS['DB']->query('SELECT * FROM cuisines;');
+        $cuisines = [];
+        foreach($returned_cuisines as $cuisine) {
+            $name = $cuisine['name'];
+            $id = $cuisine['id'];
+            $new_cuisine = new Cuisine($name, $id);
+            array_push($cuisines, $new_cuisine);
+        }
+        return $cuisines;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec('DELETE FROM cuisines');
+    }
+
+    static function find($search_id)
+    {
+        $found_cuisine = null;
+        $cuisines = self::getAll();
+        foreach($cuisines as $cuisine) {
+            $cuisine_id = $cuisine->getId();
+            if ($cuisine_id == $search_id) {
+                $found_cuisine = $cuisine;
+            }
+        }
+        return $found_cuisine;
+    }
+}
+?>
---
+++ tests/CuisineTest.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+* @backupGlobals disabled
+* @backupStaticAttributes disabled
+*/
+
+require_once 'src/Cuisine.php';
+
+$server = 'mysql:host=localhost:8889;dbname=best_restaurant_test';
+$username = 'root';
+$password = 'root';
+$DB = new PDO($server, $username, $password);
+
+class CuisineTest extends PHPUnit_Framework_TestCase
+{
+    protected function tearDown()
+    {
+        Cuisine::deleteAll();
+    }
+
+    function test_getId()
+    {
+        //Arrange
+        $name = 'Mexican';
+        $id = 1;
+        $test_Cuisine = new Cuisine($name, $id);
+
+        //Act
+        $result = $test_Cuisine->getId();
+
+        //Assert
+        $this->assertEquals(1, $result);
+    }
+
+    function test_getName()
+    {
+        //Arrange
+        $name = 'Mexican';
+        $test_Cuisine = new Cuisine($name);
+        $test_Cuisine->save();
+
+        //Act
+        $result = $test_Cuisine->getName();
+
+        //Assert
+        $this->assertEquals('Mexican', $result);
+    }
+
+    function test_save()
+    {
+        //Arrange
+        $name = 'Mexican';
+        $test_Cuisine = new Cuisine($name);
+        $test_Cuisine->save();
+
+        //Act
+        $result = Cuisine::getAll();
+
+        //Assert
+        $this->assertEquals($test_Cuisine, $result[0]);
+    }
+
+    function test_getAll()
+    {
+        //Arrange
+        $name1 = 'Mexican';
+        $name2 = 'Chinese';
+        $test_Cuisine1 = new Cuisine($name1);
+        $test_Cuisine1->save();
+        $test_Cuisine2 = new Cuisine($name2);
+        $test_Cuisine2->save();
+
+        //Act
+        $result = Cuisine::getAll();
+
+        //Assert
+        $this->assertEquals([$test_Cuisine1, $test_Cuisine2], $result);
+    }
+
+    function test_deleteAll()
+    {
+        //Arrange
+        $name1 = 'Mexican';
+        $name2 = 'Chinese';
+        $test_Cuisine1 = new Cuisine($name1);
+        $test_Cuisine1->save();
+        $test_Cuisine2 = new Cuisine($name2);
+        $test_Cuisine2->save();
+
+        //Act
+        Cuisine::deleteAll();
+        $result = Cuisine::getAll();
+
+        //Assert
+        $this->assertEquals([], $result);
+    }
+
+    function test_find()
+    {
+        //Arrange
+        $name = 'Mexican';
+        $test_Cuisine = new Cuisine($name);
+        $test_Cuisine->save();
+
+        //Act
+        $result = Cuisine::find($test_Cuisine->getId());
+
+        //Assert
+        $this->assertEquals($test_Cuisine, $result);
+    }
+
+}
+?>
