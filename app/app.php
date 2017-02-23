dd504ede1fe8cbd2d5c2471a6c34e94ee7d2e954
Katharyn Reed and Patrick McGreevy <patrick@>
2017-02-22 16:02:44 -0800

    Create routes and views for cuisines

--- app/app.php
+++ app/app.php
@@ -10,22 +10,40 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [ 'twig.path' => __DIR_
 
 $app['debug'] = true;
 
-$server = 'mysql:host=loclhost:8889;dbname=best_restaurant';
+$server = 'mysql:host=localhost:8889;dbname=best_restaurant';
 $username = 'root';
 $password = 'root';
 $DB = new PDO($server, $username, $password);
 
 $app->get('/', function() use ($app) {
-    return $app['twig']->render('root.html.twig');
+    $cuisines = Cuisine::getAll();
+
+    return $app['twig']->render('root.html.twig', array('cuisines' => $cuisines));
+});
+
+$app->post('/cuisines', function() use ($app) {
+    $name = $_POST['name'];
+    $new_cuisine = new Cuisine($name);
+    $new_cuisine->save();
+
+    return $app->redirect('/');
+});
+
+$app->get('/cuisines/{id}', function($id) use ($app) {
+    $cuisine = Cuisine::find($id);
+    return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine));
 });
 
 $app->post('/add_restaurant', function() use ($app) {
     $name = $_POST['name'];
     $cuisine_id = $_POST['cuisine'];
 
-    $new_Restaurant = new Restaurant($name, $cuisine_id);
+    $new_restaurant = new Restaurant($name, $cuisine_id);
+    $new_restaurant->save();
+    $restaurants = Restaurant::getAll();
+    $cuisine = Cuisine::find($cuisine_id);
 
-    return $app['twig']->render('root.html.twig', array('restaurant' => $restaurant));
+    return $app['twig']->render('restaurant.html.twig', array('restaurants' => $restaurants, 'cuisine' => $cuisine));
 });
 
 return $app;
